cdf67ebdbeaca0c362b5339d11ffff6f84881d4b
Annisa Puspitasari <annisap.siswa@smkn4bdg.sch.id>
2020-09-01 21:47:38 -0700

    Update Term.jsx

--- src/components/auth/Term.jsx
+++ src/components/auth/Term.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 // import ModalOtp from './Otp';
+import "../../styles/term.scss"
 
 class Term extends Component {
     state={
@@ -13,7 +14,7 @@ class Term extends Component {
                 <div className='termandcon'>
                     <div className="box">
                         <h5>SYARAT DAN KETENTUAN UMUM</h5>
-                        <p className="title">1. Kata Pengantar</p>
+                        <p className="title">Kata Pengantar</p>
                         <p className="desc">
                         Syarat dan ketentuan umum ini mengatur hak dan kewajiban yang mengikat secara hukum terhadap Pengguna untuk mengakses, menggunakan dan mengunjungi setiap dan seluruh laman situs (website) dan layanan yang terdapat pada situs www.InvestX.id ("Situs InvestX"). Situs InvestX merupakan situs milik dari PT Investasi Digital Nusantara (atau dikenal dengan nama “InvestX” atau “Penyelenggara”) yang merupakan penyelenggara layanan urun dana melalui penawaran saham berbasis teknologi informasi (equity crowdfunding) berdasarkan Peraturan POJK No.37 Tahun 2018 (selanjutnya disebut “POJK”) dan telah memperoleh izin dari Otoritas Jasa Keuangan (selanjutnya disebut OJK) No.71/D.04/2019 tentang Pemberian Izin Usaha Penyelenggara Layanan Urun Dana Melalui Penawaran Saham Berbasis Teknologi Informasi (equity crowdfunding) PT Investasi Digital Nusantara. .
                         </p>
